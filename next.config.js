3aa49699ca49251571e5598e77d1fdbef67ac9b9
paulaaa772 <pchonuoha@gmail.com>
2025-04-24 10:44:36 -0500

    Simplify Next.js config

--- next.config.js
+++ next.config.js
@@ -4,15 +4,7 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ['images.unsplash.com'],
-  },
-  env: {
-    // Hard-code the API key for now to test functionality
-    NEXT_PUBLIC_POLYGON_API_KEY: 'B2IYP3Pd1DdpKo9XSIkoQVlzp1sdDNHK',
-  },
-  // Ensure environment variables are available at build time
-  publicRuntimeConfig: {
-    NEXT_PUBLIC_POLYGON_API_KEY: process.env.NEXT_PUBLIC_POLYGON_API_KEY,
-  },
+  }
 }
 
 module.exports = nextConfig
